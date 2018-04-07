446d03bd590bc542398a4e4d1afe6cc718e535d0
Leo5878 <38134536+Leo5878@users.noreply.github.com>
2018-04-07 18:36:52 +0300

    Add files via upload

---
+++ ui/js/omw-user.js
@@ -0,0 +1,39 @@
+let DefaultUser = {
+  url: "default user url",
+}
+
+let o1 = {
+  title: "Окно",
+  parseIn: "./content.html",
+}
+
+let o2 = {
+  url: "user o2",
+  content: "<h1><center>Hello World</center></h1>",
+  title: "Трындец",
+}
+
+const userVideo = {
+  clazz: ['video leo juy'],
+  video: {
+    id: "",
+      url: "//www.w3schools.com/html/mov_bbb.mp4",
+      type: "mp4",
+    autoPlay: false,
+    controls: true,
+    height: null,
+    width: "100%",
+    poster: null,
+    loop: false,
+    muted: false,
+    preload: false,
+  },
+}
+
+const image = {
+  img: true,
+  url: "//sun1-3.userapi.com/c840629/v840629378/6edc5/SANLCQ6UDpA.jpg",
+  alt: "",
+  width: "100%",
+  height: "",
+}
