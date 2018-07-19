9b4f6dde5efafcf4edf9c9609c70b8c450233a49
Leo5878 <38134536+Leo5878@users.noreply.github.com>
2018-07-19 18:20:45 +0300

    Update omw-user.js

--- ui/js/omw-user.js
+++ ui/js/omw-user.js
@@ -1,40 +1,39 @@
 let DefaultUser = {
-  url: "default user url",
-};
+	img: true,
+}
 
 let o1 = {
-    title: "Окно",
-    parseIn: "/content.html",
+	title: "Окно",
+	parseIn: "./content.html",
 	el: "styleCombilyte",
-};
+}
 
 let o2 = {
-  url: "user o2",
-  echo: "<h1><center>Hello World</center></h1>",
-  title: "Open window",
-};
+	url: "user o2",
+	echo: "<h1><center>Hello World</center></h1>",
+	title: "Open window",
+}
 
 const userVideo = {
-  el: ['video leo juy'],
-  video: {
-    id: "",
-	  url: "//www.w3schools.com/html/mov_bbb.mp4",
-    type: "mp4",
-    autoPlay: false,
-    controls: true,
-    height: null,
-    width: "100%",
-    poster: null,
-    loop: false,
-    muted: false,
-    preload: false,
-  },
-};
+	el: ['video leo juy'],
+	video: {
+		id: "",
+		type: "mp4",
+		url: "//www.w3schools.com/html/mov_bbb.mp4",
+		autoPlay: false,
+		controls: true,
+		height: null,
+		width: "100%",
+		poster: null,
+		loop: false,
+		muted: false,
+		preload: false,
+	},
+}
 
 const image = {
-  img: true,
-  url: "//sun1-3.userapi.com/c840629/v840629378/6edc5/SANLCQ6UDpA.jpg",
-  alt: "",
-  width: "100%",
-  height: "",
-};
+	url: "//sun1-3.userapi.com/c840629/v840629378/6edc5/SANLCQ6UDpA.jpg",
+	alt: "",
+	width: "100%",
+	height: "",
+}
